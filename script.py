59c797a092e9afb1386df93868ccf99ee9ab5579
Tomas Ukrinas <t.ukrinas@ba.lt>
2019-10-09 10:53:58 +0300

    needed changes

--- script.py
+++ script.py
@@ -1,7 +1,3 @@
 from Bio import SeqIO
-from myfunc import make_protein_record
-from reallyMyFunc import myfunc
-proteins = (make_protein_record(seq_record) for seq_record in \
-SeqIO.parse("C:\\Users\\tukrinas\\Documents\\ManoProjektai\\BioInformatika\\1 lab\\ls_orchid.fasta", "fasta"))
-SeqIO.write(proteins, "translations.fasta", "fasta")
+from reallyMyFunc import myFunc
 myFunc("BMW", "res.txt")
