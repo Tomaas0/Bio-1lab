56a398600d3d9b0e44ed25bedaf34b97be01a839
Tomas Ukrinas <t.ukrinas@ba.lt>
2019-10-09 23:57:53 +0300

    Uuuu funkcijyte

--- reallyMyFunc.py
+++ reallyMyFunc.py
@@ -1,3 +1,29 @@
-def myFunc(input, file):
-    failas = open(file, "a")
-    failas.write(input)
+from Bio import SeqIO
+
+def myFunc(file):
+
+    bpLen = 100
+
+    startCodon = "ATG"
+    stopCodons = ["TAG", "TAA", "TGA"]
+
+    orfs = []
+
+    for sequence in SeqIO.parse(file, "fasta"):
+        frame = sequence.seq
+        inside = False
+        orf = ""
+        for i in range(round(len(frame) / 3)):
+            triplet = frame[i * 3:(i * 3) + 3]
+            if inside == True and triplet not in stopCodons:
+                orf += triplet
+            elif inside == True and triplet in stopCodons:
+                orf += triplet
+                if len(orf) > bpLen:
+                    orfs.append(orf)
+                orf = ""
+                inside = False
+            elif inside == False and triplet == startCodon:
+                inside = True
+                orf += triplet
+    return orfs
--- script.py
+++ script.py
@@ -1,3 +1,13 @@
-from Bio import SeqIO
 from reallyMyFunc import myFunc
-myFunc("BMW", "res.txt")
+
+orfs = []
+
+for file in ["plazmide.fasta", "ls_orchid.fasta"]:
+    fileOrfs = myFunc(file)
+    for orf in fileOrfs:
+        orfs.append(orf)
+
+file = open("res.txt", "w")
+for orf in orfs:
+    file.write(str(orf))
+    file.write("\n")
