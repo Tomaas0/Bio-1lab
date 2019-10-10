c54c033911afd6efd75d8d65560d3c63cea1f5a2
Tomas Ukrinas <t.ukrinas@ba.lt>
2019-10-10 08:26:26 +0300

    Get proteins func

--- myfunc.py
+++ myfunc.py
@@ -1,7 +1,6 @@
 from Bio.SeqRecord import SeqRecord
 def make_protein_record(nuc_record):
-    print (nuc_record)
     """Returns a new SeqRecord with the translated sequence (default table)."""
-    return SeqRecord(seq = nuc_record.seq.translate(cds=True), \
-                     id = "trans_" + nuc_record.id, \
+    return SeqRecord(seq = nuc_record.translate(cds=True), \
+                     id = "translation", \
                      description = "translation of CDS, using default table")
--- script.py
+++ script.py
@@ -1,13 +1,22 @@
 from reallyMyFunc import myFunc
+from myfunc import make_protein_record
 
 orfs = []
+proteins = []
 
 for file in ["plazmide.fasta", "ls_orchid.fasta"]:
     fileOrfs = myFunc(file)
     for orf in fileOrfs:
         orfs.append(orf)
+        proteins.append(make_protein_record(orf))
 
 file = open("res.txt", "w")
 for orf in orfs:
     file.write(str(orf))
+    file.write("\n")
+
+file.write("\n")
+    
+for protein in proteins:
+    file.write(str(protein.seq))
     file.write("\n")
